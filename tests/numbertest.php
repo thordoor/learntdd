aa2f36aabc7563b2939ae165fcd4364d1bbb3bcd
thordoor <thor@colourbox.com>
2017-11-24 09:16:59 +0100

    still breathing

--- tests/numbertest.php
+++ tests/numbertest.php
@@ -1,37 +1,5 @@
-<?php
-use PHPUnit\Framework\TestCase;
-require "app/fractions.php";
+Fractions
+ [x] Add fractions
+ [x] Convert to fraction if integer
+ [x] Convert to fraction and calculate
 
-class FractionsTest extends TestCase
-{
-    private $fraction;
-
-    protected function setup()
-    {
-        $this->fraction = new Fractions();
-    }
-
-    protected function tearDown()
-    {
-        $this->calculator = NULL;
-    }
-
-    public function testAddFractions()
-    {
-        $result = $this->fraction->addFractions(1/1, 1/2);
-        $this->assertEquals('3/2', $result);
-    }
-
-    public function testConvertToFractionIfInteger()
-    {
-        $result = $this->fraction->convertToFractionIfInteger(5);
-        $this->assertEquals('5/1', $result);
-    }
-
-    public function testConvertToFractionAndCalculate()
-    {
-        $result = $this->fraction->convertToFractionAndCalculate(5.0);
-        $this->assertEquals('5/1', $result);
-    }
-}
-?>
